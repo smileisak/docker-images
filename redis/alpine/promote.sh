91bbd017e0163151cd3412e15d16ca9f35196497
MTreide-Heuser <56307225+MTreide-Heuser@users.noreply.github.com>
2019-11-19 14:46:02 +0100

    Update promote.sh
    fixup

--- redis/alpine/promote.sh
+++ redis/alpine/promote.sh
@@ -8,7 +8,7 @@ for i in {1..10}; do
   # Convert the IP of the promoted pod to a hostname
   MASTERPOD=$(kubectl get pod -o jsonpath='{range .items[*]}{.metadata.name} {..podIP} {.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}' -l redis-role=slave --sort-by=.metadata.name|grep True|awk '{print $1}')
 
-  [[ ! -z "${MASTERPOD}" ]] && break
+  [[ -n "${MASTERPOD}" ]] && break
   [[ "${i}" -eq "10" ]] && exit 1
 done
 
