84c39e3b64f03e8bec008c22788022d6b6b7a354
MTreide-Heuser <56307225+MTreide-Heuser@users.noreply.github.com>
2019-11-19 14:45:35 +0100

    Update promote.sh
    Make it a bit more fault tollerant

--- redis/alpine/promote.sh
+++ redis/alpine/promote.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 MASTERIP=$6
+unset MASTERPOD
 
-# Convert the IP of the promoted pod to a hostname
-MASTERPOD=`kubectl get pod -o jsonpath='{range .items[*]}{.metadata.name} {..podIP} {.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}' -l redis-role=slave --sort-by=.metadata.name|grep True|awk '{print $1}'`
+for i in {1..10}; do
+  # No turbo loop here
+  sleep 2
+  # Convert the IP of the promoted pod to a hostname
+  MASTERPOD=$(kubectl get pod -o jsonpath='{range .items[*]}{.metadata.name} {..podIP} {.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}' -l redis-role=slave --sort-by=.metadata.name|grep True|awk '{print $1}')
+
+  [[ ! -z "${MASTERPOD}" ]] && break
+  [[ "${i}" -eq "10" ]] && exit 1
+done
+
 echo "PROMO ARGS: $@"
 echo "PROMOTING $MASTERPOD ($MASTERIP) TO MASTER"
 kubectl label --overwrite pod $MASTERPOD redis-role="master"
